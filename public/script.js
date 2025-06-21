b8387109d5ba59026ce7dfa0255f547647e91111
Mayank Sharma <mayank.sharma.dev06@gmail.com>
2025-06-21 07:39:52 +0530

    Clear a bug

--- public/script.js
+++ public/script.js
@@ -5,17 +5,3 @@ linkbtn.addEventListener("click",()=>{
    dropDown.classList.toggle("show");
 });
 
-  // Yeh tab chale jab screen chhoti ho
-//   if (window.innerWidth <= 750) {
-//     const topperTrigger = document.getElementById("topper-class");
-
-//     topperTrigger.addEventListener("click", function (e) {
-//       e.stopPropagation(); // Prevent bubbling
-//       const submenu = this.querySelector(".topper");
-//       submenu.classList.toggle("show-topper");
-//     });
-//        document.addEventListener("click", function () {
-//       const openMenu = document.querySelector(".topper.show-topper");
-//       if (openMenu) openMenu.classList.remove("show-topper");
-//     });
-//   }
